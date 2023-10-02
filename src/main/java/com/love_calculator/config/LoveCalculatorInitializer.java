33fc2a09e0e166ddd496dd121943151277c87f32
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-03 01:05:24 +0500

    DispatcherServlet Initalized by using spring-servlet.xml successfully

--- src/main/java/com/love_calculator/config/LoveCalculatorInitializer.java
+++ src/main/java/com/love_calculator/config/LoveCalculatorInitializer.java
@@ -2,14 +2,29 @@ package com.love_calculator.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class LoveCalculatorInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println("Custom on startup method called.....");
+
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocation("classpath:spring-servlet.xml");
+		
+		// create a dispatcher servlet object 
+		
+		DispatcherServlet dispatcher = new DispatcherServlet(context); 
+		
+		// register dispatcher servlet with servlet context 
+		ServletRegistration.Dynamic myCustomDispatcherName = servletContext.addServlet("myDispatcher", dispatcher);
+		
+		myCustomDispatcherName.setLoadOnStartup(1);
+		myCustomDispatcherName.addMapping("/");
 	}
 
 }
