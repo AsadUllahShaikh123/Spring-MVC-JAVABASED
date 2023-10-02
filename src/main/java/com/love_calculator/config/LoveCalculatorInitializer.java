724223a22177d079f3fc7f3cc7526a2bffe2b98b
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-03 01:21:49 +0500

    deleted spring-servlet.xml file and configure dispacther java based file

--- src/main/java/com/love_calculator/config/LoveCalculatorInitializer.java
+++ src/main/java/com/love_calculator/config/LoveCalculatorInitializer.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,18 +14,32 @@ public class LoveCalculatorInitializer implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 
-		XmlWebApplicationContext context = new XmlWebApplicationContext();
-		context.setConfigLocation("classpath:spring-servlet.xml");
+		// ============= For WEB BAsed ===============
+//		XmlWebApplicationContext context = new XmlWebApplicationContext();
+//		context.setConfigLocation("classpath:spring-servlet.xml");
+		
+		
+		
+		// ============= For Java Based ================
+		AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+		webApplicationContext.register(LoveCalculatorAppConfig.class);
+		
 		
 		// create a dispatcher servlet object 
 		
-		DispatcherServlet dispatcher = new DispatcherServlet(context); 
+		DispatcherServlet dispatcher = new DispatcherServlet(webApplicationContext); 
 		
 		// register dispatcher servlet with servlet context 
 		ServletRegistration.Dynamic myCustomDispatcherName = servletContext.addServlet("myDispatcher", dispatcher);
 		
 		myCustomDispatcherName.setLoadOnStartup(1);
 		myCustomDispatcherName.addMapping("/");
+		
+		
+		
+		
+		
+		
 	}
 
 }
